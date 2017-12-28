336181654a463e4fdcec97cae18454bc63309292
Lei Hao <leihao1313@gmail.com>
2017-12-28 00:10:49 -0500

    finish lab01

---
+++ lab01/lab01.py
@@ -0,0 +1,78 @@
+"""Lab 1: Expressions and Control Structures"""
+
+# Coding Practice
+
+def repeated(f, n, x):
+    """Returns the result of composing f n times on x.
+
+    >>> def square(x):
+    ...     return x * x
+    ...
+    >>> repeated(square, 2, 3)  # square(square(3)), or 3 ** 4
+    81
+    >>> repeated(square, 1, 4)  # square(4)
+    16
+    >>> repeated(square, 6, 2)  # big number
+    18446744073709551616
+    >>> def opposite(b):
+    ...     return not b
+    ...
+    >>> repeated(opposite, 4, True)
+    True
+    >>> repeated(opposite, 5, True)
+    False
+    >>> repeated(opposite, 631, 1)
+    False
+    >>> repeated(opposite, 3, 0)
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n<=0:
+        return 
+    elif n==1:
+        return f(x)
+    else:
+        return f(repeated(f,n-1,x))
+
+def sum_digits(n):
+    """Sum all the digits of n.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    """
+    "*** YOUR CODE HERE ***"
+    if n<10:
+        return n
+    else :
+        lastdigit = n % 10
+        restdigits = n // 10
+        return lastdigit + sum_digits(restdigits)
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    """
+    if n< 88 :
+        return False
+    else:
+        lastdigit=n%10
+        n=n//10
+        secondlast=n%10
+        return lastdigit==secondlast or double_eights(n)
+    """
+    return False if n<88 else n%10==(n//10)%10 or double_eights(n//10)
