9b4cbb8c4e3b6c5b85c46cfd9d3c05939efa8abd
Lei Hao <leihao1313@gmail.com>
2018-01-01 11:47:36 -0500

    add hw02 hw03 lab02 lab03

---
+++ lab03/lab03.py
@@ -0,0 +1,43 @@
+def ab_plus_c(a, b, c):
+    """Computes a * b + c.
+
+    >>> ab_plus_c(2, 4, 3)  # 2 * 4 + 3
+    11
+    >>> ab_plus_c(0, 3, 2)  # 0 * 3 + 2
+    2
+    >>> ab_plus_c(3, 0, 2)  # 3 * 0 + 2
+    2
+    """
+    "*** YOUR CODE HERE ***"
+
+def gcd(a, b):
+    """Returns the greatest common divisor of a and b.
+    Should be implemented using recursion.
+
+    >>> gcd(34, 19)
+    1
+    >>> gcd(39, 91)
+    13
+    >>> gcd(20, 30)
+    10
+    >>> gcd(40, 40)
+    40
+    """
+    "*** YOUR CODE HERE ***"
+
+def hailstone(n):
+    """Print out the hailstone sequence starting at n, and return the
+    number of elements in the sequence.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    """
+    "*** YOUR CODE HERE ***"
